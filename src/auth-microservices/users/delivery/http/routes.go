40a11c9476efeff66475d55e86caaba24e776a1a
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-06-18 12:05:54 +0200

    feat: uncomment the getall route and rename it from get to getall

--- src/auth-microservices/users/delivery/http/routes.go
+++ src/auth-microservices/users/delivery/http/routes.go
@@ -8,6 +8,6 @@ func (uc UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
 	userroute := rg.Group("/auth")
 	userroute.POST("/signup", uc.SignUp)
 	userroute.POST("signin", uc.SignIn)
-	// userroute.GET("/get", uc.GetAll)
+	userroute.GET("/getall", uc.GetAll)
 	userroute.GET("/get", uc.GetOne)
 }
