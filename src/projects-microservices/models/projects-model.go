21897b8cdbc82266f3bd266cd5d1caf4ed68d4a8
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-06-24 17:25:23 +0200

    feat: update the model to remove the validation to the author column

--- src/projects-microservices/models/projects-model.go
+++ src/projects-microservices/models/projects-model.go
@@ -3,5 +3,5 @@ package projects_model
 type Project struct {
 	Name        string `json:"name" validate:"required" binding:"required"`
 	Description string `json:"description" validate:"required" binding:"required"`
-	Author      string `validate:"required" binding:"required"`
+	Author      string
 }
