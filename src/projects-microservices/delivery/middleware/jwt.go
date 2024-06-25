020c50d673b46592f483286241ecccdda52fcd23
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-06-25 15:20:21 +0200

    fix: the ExtractUnverifiedClaims function

--- src/projects-microservices/delivery/middleware/jwt.go
+++ src/projects-microservices/delivery/middleware/jwt.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 	helper "traileau-projects-microservices/helpers"
 
@@ -10,22 +9,19 @@ import (
 
 func Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		clientToken := c.Request.Header.Get("token")
-		if clientToken == "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization Header Provided")})
+		token := helper.ExtractToken(c)
+
+		_, err := helper.ExtractUnverifiedClaims(token)
+
+		if err != nil {
+			c.JSON(http.StatusBadGateway, gin.H{"message": "zizi"})
 			c.Abort()
 			return
 		}
 
-		claims, err := helper.ValidateToken(clientToken, c)
-		if err != "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-			c.Abort()
-			return
-		}
-		c.Set("email", claims.Email)
-		c.Set("first_name", claims.Username)
-		c.Set("user_type", claims.User_type)
-		c.Next()
+		// c.Set("email", claims.Email)
+		// c.Set("first_name", claims.Username)
+		// c.Set("user_type", claims.User_type)
+		// c.Next()
 	}
 }
--- src/projects-microservices/helpers/jwt.go
+++ src/projects-microservices/helpers/jwt.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type SignedDetails struct {
@@ -17,33 +19,38 @@ type SignedDetails struct {
 
 var SECRET_KEY string = os.Getenv("SECRET_KEY")
 
-func ExtractToken(ctx *gin.Context) (claims *SignedDetails, msg string) {
-	token, err := ctx.Request.Header["Authorization"]
-
-	if !err {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Token not found"})
-		ctx.Abort()
-		return
+func ExtractUnverifiedClaims(tokenString string) (string, error) {
+	var name string
+	parsedToken := strings.Split(tokenString, "Bearer ")
+	token, _, err := new(jwt.Parser).ParseUnverified(parsedToken[1], jwt.MapClaims{})
+	if err != nil {
+		fmt.Println(err)
+		return "", err
 	}
 
-	fmt.Println(token)
+	if tokenString == "" {
+		fmt.Println("Error missing token from params")
+		return "", err
+	}
 
-	// if err != true {
-	// 	msg = err.Error()
-	// 	return
-	// }
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		name = fmt.Sprintln(claims["sub"])
+	}
 
-	// claims, ok := token.Claims.(*SignedDetails)
-	// if !ok {
-	// 	msg = fmt.Sprintf("the token is invalid")
-	// 	msg = err.Error()
-	// 	return
-	// }
+	if name == "" {
+		return "", fmt.Errorf("invalid token payload")
+	}
+	return name, nil
+}
 
-	// if claims.ExpiresAt < time.Now().Local().Unix() {
-	// 	msg = fmt.Sprintf("token is expired")
-	// 	msg = err.Error()
-	// 	return
-	// }
-	return claims, msg
+func ExtractToken(ctx *gin.Context) string {
+	token := ctx.Request.Header.Get("Authorization")
+
+	if token == "" {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Token not found"})
+		ctx.Abort()
+		return "nul"
+	}
+
+	return token
 }
