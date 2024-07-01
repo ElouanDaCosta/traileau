d22fffe377a0f84f039d01fe88796d44bdc90fe9
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-07-01 22:21:15 +0200

    feat: add the GetUser function and add global variable with env variable

---
+++ src/projects-microservices/delivery/http/auth-microservice.go
@@ -0,0 +1,46 @@
+package http
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+var dotenv = godotenv.Load()
+
+var (
+	auth_baseUrl       string = os.Getenv("AUTH_API_URL")
+	auth_version       string = os.Getenv("AUTH_API_VERSION")
+	auth_port          string = os.Getenv("AUTH_PORT")
+	auth_route         string = os.Getenv("AUTH_ROUTE")
+	authUrl            string = auth_baseUrl + auth_port + "/v" + auth_version + "/" + auth_route + "/"
+	getoneuserEndpoint string = os.Getenv("AUTH_GET_ONE_USER_ENDPOINT")
+)
+
+func init() {
+	if dotenv != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func GetUser(email string) error {
+
+	temp := strings.Split(email, "\n")
+
+	resp, err := http.Get(authUrl + getoneuserEndpoint + temp[0])
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	fmt.Println(resp)
+
+	fmt.Println("Request send to the authentication microservice at:", auth_baseUrl, auth_port)
+
+	return err
+}
