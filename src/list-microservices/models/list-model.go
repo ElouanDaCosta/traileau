bcbcf4f9ede0211de9e73e33911e1c29efdb71a7
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-07-28 11:49:23 +0200

    feat: add the Boards property on the list-model

--- src/list-microservices/models/list-model.go
+++ src/list-microservices/models/list-model.go
@@ -3,4 +3,5 @@ package list_model
 type List struct {
 	Name     string `json:"name" validate:"required" binding:"required"`
 	Position int    `json:"position" binding:"required"`
+	Boards   string `json:"boards" binding:"required"`
 }
